40a7928168e5098804accfb6857490cbd531af24
Rajih kharissuha <rajhk704@gmail.com>
2026-04-02 19:29:13 +0700

    Add Code Google Analytic

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -4,6 +4,6 @@ import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function Analytics() {
   return (
-    <GoogleAnalytics gaMeasurementId="G-XXXXXXXXXX" trackPageViews={true} />
+    <GoogleAnalytics gaMeasurementId="G-C4Y99PY5C0" trackPageViews={true} />
   );
 }
